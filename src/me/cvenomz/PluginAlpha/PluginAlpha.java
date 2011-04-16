3d066dbd757c3ff869102366fceb9e7a0ecc637a
Mr. cvenomz <cvenomz.email@gmail.com>
2011-04-15 21:25:05 -0400

    first with some code

---
+++ src/me/cvenomz/PluginAlpha/PluginAlpha.java
@@ -0,0 +1,19 @@
+package me.cvenomz.PluginAlpha;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginAlpha extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
